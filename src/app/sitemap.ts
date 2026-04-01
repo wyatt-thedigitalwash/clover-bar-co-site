c78c10ada62057f91999adb881fa62ee34db0593
WyattBensman <wyatt@thedigitalwash.com>
2026-04-01 19:31:16 -0400

    Initial commit

---
+++ src/app/sitemap.ts
@@ -0,0 +1,9 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://DOMAIN.com';
+
+  return [
+    { url: baseUrl, changeFrequency: 'weekly', priority: 1.0 },
+  ].map(page => ({ ...page, lastModified: new Date() }));
+}
